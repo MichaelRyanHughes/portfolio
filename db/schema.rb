494db1726c0714f23f2bdf4f57b35f577a9f9792
Michael Hughes <MichaelRyanHughes@gmail.com>
2014-06-15 11:40:12 -0700

    work in progress

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140603003418) do
+ActiveRecord::Schema.define(version: 20140605233324) do
+
+  create_table "albums", force: true do |t|
+    t.text     "title"
+    t.text     "description"
+    t.string   "artwork"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "photos", force: true do |t|
     t.string   "url"
