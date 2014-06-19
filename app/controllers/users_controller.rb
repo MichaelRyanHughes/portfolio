bc3b7d6b05121e23da8add39d67ed8ddfe4eec83
Michael Hughes <MichaelRyanHughes@gmail.com>
2014-06-19 10:43:50 -0700

    update temp data for album model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
-class UsersController < ApplicationController 
+class UsersController < ApplicationController
+  
+  before_filter :authenticate_user!
+  
+  def edit
+    @albums = Album.all
+  end 
 end
