f120bf3075b0cb4d87387346ef0107d8adc7e2ba
Michael Hughes <MichaelRyanHughes@gmail.com>
2014-07-26 13:24:31 -0700

    regenerate Gemfile.lock and schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619220130) do
+ActiveRecord::Schema.define(version: 20140714224410) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
   create_table "albums", force: true do |t|
     t.text     "title"
     t.text     "description"
     t.string   "artwork"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-
-  create_table "photos", force: true do |t|
-    t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image_file_name"
@@ -66,6 +62,7 @@ ActiveRecord::Schema.define(version: 20140619220130) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
 end
