12b2e7c2beb2132fdfdabc57398687561d6eb952
Michael Hughes <MichaelRyanHughes@gmail.com>
2014-07-26 17:46:06 -0700

    remove un-used photos model

--- app/models/photo.rb
+++
@@ -1,9 +0,0 @@
-class Photo < ActiveRecord::Base
-  #def get_photo_url
-    #url = "https://api.instagram.com/v1/users/234675123/media/recent/?client_id=64cabc37213b4eb6bc6853af9816d4e2"
-    #uri = URI.parse(url)
-    #json_response = Net::HTTP.get_response(uri)
-    #hash = JSON.parse json_response.body
-    #url = hash["data"].map{|id| id["images"]["standard_resolution"]["url"]}
-  #end
-end
