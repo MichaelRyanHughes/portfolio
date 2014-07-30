e95a4382c8cc3ae08b6df1ed7c54825dcb477518
Michael Hughes <MichaelRyanHughes@gmail.com>
2014-07-29 19:57:50 -0700

    add js popup video player

---
+++ app/assets/javascripts/video_popup.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.play-button').magnificPopup({
+    type:'inline',
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+});
